98fd552c35583eb9b802902a878929337e397084
Michael P <52107159+Artzert2215@users.noreply.github.com>
2026-05-30 17:49:35 +0200

    components & some tweaks

---
+++ components.js
@@ -0,0 +1,29 @@
+class SiteHeader extends HTMLElement {
+  connectedCallback() {
+    this.outerHTML = `
+    <header>
+        <q>Visit <a href="https://www.022150.xyz/">www.022150.xyz</a> today!</q>
+        <hr>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+    `;
+  }
+}
+customElements.define('site-header', SiteHeader);
+
+class SiteFooter extends HTMLElement {
+  connectedCallback() {
+    this.outerHTML = `
+    <footer>
+        <p><b>Built without ads or trackers.</b></p>
+        <p>Like this style? Feel free to use it yourself!<br><a href="https://github.com/Artzert2215/www.022150.xyz">View the source code of this site.</a></p>
+        <small>Last update: 30 May 2026</small>
+    </footer>
+    `;
+  }
+}
+customElements.define('site-footer', SiteFooter);
